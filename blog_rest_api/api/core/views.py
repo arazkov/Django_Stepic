f55fb55b2d4b4f0a33ad5bc6ff9e38a6c879d89d
Vladimir <arazkov@ya.ru>
2021-07-14 00:17:10 +0300

    V 0.1.3 add to view posts from tags and all tags

--- blog_rest_api/api/core/views.py
+++ blog_rest_api/api/core/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
-from .serializers import PostSerializer
+from .serializers import PostSerializer, TagSerializer
 from .models import Post
-from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import pagination
+from rest_framework import generics
+from taggit.models import Tag
 
 
 class PageNumberSetPagination(pagination.PageNumberPagination):
@@ -18,3 +19,20 @@ class PostViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
     pagination_class = PageNumberSetPagination
+
+
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
+
+
+class TagView(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.AllowAny]
